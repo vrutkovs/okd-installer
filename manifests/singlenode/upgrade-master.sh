392be76a8ff9f47229ff961724a140618aae0115
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-01-25 09:40:15 +0100

    singlenode: use `$(hostname -f)` for node name

--- manifests/singlenode/upgrade-master.sh
+++ manifests/singlenode/upgrade-master.sh
@@ -4,15 +4,12 @@
 
 set -x
 
-# Get master node name
-NODE=$(oc get node -l node-role.kubernetes.io/worker= -o=jsonpath='{.items[*].metadata.name}')
-
 # Fetch expected master config
 MC=$(oc get mcp/master -o=jsonpath='{.spec.configuration.name}')
 oc get "mc/${MC}" -o yaml > /tmp/mc.yaml
 
 # Annotate the node as if it has succeeded
-oc annotate "node/${NODE}" "machineconfiguration.openshift.io/desiredConfig"="${MC}" --overwrite
+oc annotate "node/$(hostname -f)" "machineconfiguration.openshift.io/desiredConfig"="${MC}" --overwrite
 
 # Pivot to new machine config and reboot
-/run/bin/machine-config-daemon start --root-mount=/ --node-name="${NODE}" --once-from /tmp/mc.yaml
+/run/bin/machine-config-daemon start --root-mount=/ --node-name="$(hostname -f)" --once-from /tmp/mc.yaml
